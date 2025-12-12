a2d59a2c9d91841ea690de40acd7f9457b7fe622
EC2 Default User <ec2-user@ip-172-31-4-133.ap-south-1.compute.internal>
2025-12-12 06:01:54 +0000

    another change

--- install_nginx.sh
+++ install_nginx.sh
@@ -9,7 +9,7 @@ sudo systemctl enable nginx
 sudo systemctl start nginx
 
 echo "📄 Copying HTML file to NGINX root..."
-sudo cp /home/ec2-user/project-2/index.html /usr/share/nginx/html/index.html
+sudo cp /home/ec2-user/project-3/index.html /usr/share/nginx/html/index.html
 
 echo "✅ Deployment complete! NGINX is running."
 
